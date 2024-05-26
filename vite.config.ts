004b65714ed6c6aace31a8692bd1afb9c15316a5
alexcibotari <alexandru.cibotari@gmail.com>
2024-05-26 23:01:20 +0200

    update utils

---
+++ vite.config.ts
@@ -0,0 +1,18 @@
+import { defineConfig } from 'vite';
+
+export default defineConfig({
+  optimizeDeps: {
+    exclude: ['node-fetch']
+  },
+  build: {
+    rollupOptions: {
+      external: ['node-fetch']
+    },
+    lib: {
+      entry: './src/index.ts',
+      fileName: 'index',
+      name: 'localess',
+      formats: ['es', 'cjs', 'umd'],
+    }
+  }
+})
