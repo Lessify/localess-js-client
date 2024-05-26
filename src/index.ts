9fc8718a0c8c9858c14e7737657fe47acbce6ce7
alexcibotari <alexandru.cibotari@gmail.com>
2024-05-26 20:08:15 +0200

    change name for options

--- src/index.ts
+++ src/index.ts
@@ -6,12 +6,27 @@ import {Content, ContentAsset, Links, Translations} from "./models";
 export * from './models';
 
 export type LocalessClientOptions = {
-  domain: string;
+  /**
+   * A fully qualified domain name with protocol (http/https) and port
+   * Example: https://localess-prod.web.app
+   */
+  origin: string;
+  /**
+   * Content version to fetch, leave empty for 'published' or 'draft' for the latest draft
+   */
   version?: 'draft';
-  location?: string;
+  /**
+   * Locale to fetch content in, leave empty for default locale
+   */
+  locale?: string;
+  /**
+   * Localess space ID, cna be found in the Localess Space settings
+   */
   spaceId: string;
+  /**
+   * Localess API token, can be found in the Localess Space settings
+   */
   token: string;
-  secure?: boolean;
 }
 
 export function localessClient(options: LocalessClientOptions) {
@@ -21,44 +36,43 @@ export function localessClient(options: LocalessClientOptions) {
   if (proxyURIFromEnv()) {
     fetchOptions.agent = new ProxyAgent();
   }
-  const host = `${options.secure ? 'https' : 'http'}://${options.domain}`;
   const version = options.version ? `&version=${options.version}` : '';
-  const locale = options.location ? `&location=${options.location}` : '';
+  const locale = options.locale ? `&locale=${options.locale}` : '';
   return {
 
     async getLinks(): Promise<Links> {
-      const response = await fetch(`${host}/api/v1/spaces/${options.spaceId}/links?token=${options.token}`, fetchOptions)
+      const response = await fetch(`${origin}/api/v1/spaces/${options.spaceId}/links?token=${options.token}`, fetchOptions)
       const data = await response.json();
       return data as Links;
     },
 
     async getContentBySlug(slug: string): Promise<Content> {
-      const response = await fetch(`${host}/api/v1/spaces/${options.spaceId}/contents/slugs/${slug}?token=${options.token}${version}${locale}`, fetchOptions)
+      const response = await fetch(`${origin}/api/v1/spaces/${options.spaceId}/contents/slugs/${slug}?token=${options.token}${version}${locale}`, fetchOptions)
       const data = await response.json();
       return data as Content;
     },
 
     async getContentById(id: string): Promise<Content> {
-      const response = await fetch(`${host}/api/v1/spaces/${options.spaceId}/contents/${id}?token=${options.token}${version}${locale}`, fetchOptions)
+      const response = await fetch(`${origin}/api/v1/spaces/${options.spaceId}/contents/${id}?token=${options.token}${version}${locale}`, fetchOptions)
       const data = await response.json();
       return data as Content;
     },
 
     async getTranslations(): Promise<Translations> {
-      const response = await fetch(`${host}/api/v1/spaces/${options.spaceId}/translations/${locale || 'en'}`, fetchOptions)
+      const response = await fetch(`${origin}/api/v1/spaces/${options.spaceId}/translations/${locale || 'en'}`, fetchOptions)
       const data = await response.json();
       return data as Translations;
     },
 
     syncScriptUrl(): string {
-      return `${host}/scripts/sync-v1.js`
+      return `${origin}/scripts/sync-v1.js`
     },
 
     assetLink(asset: ContentAsset | string): string {
       if (typeof asset === 'string') {
-        return `${host}/api/v1/spaces/${options.spaceId}/assets/${asset}`;
+        return `${origin}/api/v1/spaces/${options.spaceId}/assets/${asset}`;
       } else {
-        return `${host}/api/v1/spaces/${options.spaceId}/assets/${asset.uri}`;
+        return `${origin}/api/v1/spaces/${options.spaceId}/assets/${asset.uri}`;
       }
     }
   }
