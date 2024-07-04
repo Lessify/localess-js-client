4d232a46e01156a15740e18d23cbf1c989e84df7
alexcibotari <alexandru.cibotari@gmail.com>
2024-07-04 09:52:19 +0200

    Add Error Logs

--- src/index.ts
+++ src/index.ts
@@ -117,7 +117,6 @@ export function localessClient(options: LocalessClientOptions) {
         const data = await response.json();
         return data as Content;
       } catch (error: any){
-        console.error(LOG_GROUP, 'getContentBySlug error code :', error.code, error.type, error.errno);
         console.error(LOG_GROUP, 'getContentBySlug error :', error);
         return {} as Content;
       }
