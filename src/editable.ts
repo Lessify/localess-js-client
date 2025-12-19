651f3688c3a937061d7da9dd4513b2393614d810
alexcibotari <alexandru.cibotari@gmail.com>
2025-12-19 20:40:38 +0100

    deprecated some functions

--- src/editable.ts
+++ src/editable.ts
@@ -1,5 +1,23 @@
 import {ContentDataSchema} from "./models";
 
+/**
+ * Adds Localess editable attributes to a content item.
+ * @param content
+ * @returns An object containing data-ll-id and data-ll-schema attributes.
+ */
+export function localessEditable(content: ContentDataSchema ) {
+  return {
+    'data-ll-id': content._id,
+    'data-ll-schema': content._schema || content.schema,
+  }
+}
+
+/**
+ * Adds Localess editable attributes to a content item.
+ * @param content
+ * @returns An object containing data-ll-id and data-ll-schema attributes.
+ * @deprecated use localessEditable instead
+ */
 export function llEditable(content: ContentDataSchema ) {
   return {
     'data-ll-id': content._id,
@@ -7,6 +25,24 @@ export function llEditable(content: ContentDataSchema ) {
   }
 }
 
+/**
+ * Adds Localess editable field attribute to a specific field.
+ * @param fieldName
+ * @returns An object containing data-ll-field attribute.
+ */
+
+export function localessEditableField(fieldName: string ) {
+  return {
+    'data-ll-field': fieldName,
+  }
+}
+
+/**
+ * Adds Localess editable field attribute to a specific field.
+ * @param fieldName
+ * @returns An object containing data-ll-field attribute.
+ * @deprecated use localessEditableField instead
+ */
 export function llEditableField(fieldName: string ) {
   return {
     'data-ll-field': fieldName,
